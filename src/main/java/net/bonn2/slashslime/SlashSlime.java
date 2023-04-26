7f9a657092748c78175d8036925f7510c5043c62
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2023-04-26 10:21:52 -0400

    Initial commit

---
+++ src/main/java/net/bonn2/slashslime/SlashSlime.java
@@ -0,0 +1,17 @@
+package net.bonn2.slashslime;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SlashSlime extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
